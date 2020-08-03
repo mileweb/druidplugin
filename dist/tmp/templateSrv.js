a74f7bc780aec622717c70ad6b06d9832d21c170
ZhanHui-Xu <zhanhui.xu@quantil.com>
2020-08-03 11:23:10 +0800

    m

--- dist/tmp/templateSrv.js
+++
@@ -1,9 +0,0 @@
-var templateSrv = {
-  builtIns: {
-    __interval:{text: "1s", value: "1s"},
-    __interval_ms:{text: "100", value: "100"}
-  },
-  grafanaVariables: {},
-  index:{},
-  regex:/\$(\w+)|\[\[([\s\S]+?)(?::(\w+))?\]\]|\${(\w+)(?::(\w+))?}/g
-}
