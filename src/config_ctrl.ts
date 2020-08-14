ac924baef9b674da4c89074b447e845b8914c85c
ZhanHui-Xu <zhanhui.xu@quantil.com>
2020-08-14 17:03:25 +0800

    m

--- src/config_ctrl.ts
+++ src/config_ctrl.ts
@@ -6,10 +6,23 @@ import _ from 'lodash';
 export class DruidConfigCtrl {
   static templateUrl = 'partials/config.html';
   current: any;
+  listDataSources: any;
+  datasource: any;
 
+  
   constructor($scope) {
     // this.current.jsonData.adhocFilterDS = this.current.jsonData.adhocFilterDS;
     // this.current.jsonData.periodGranularity = this.current.jsonData.periodGranularity;    
+
+    // needs to be defined here as it is called from typeahead
+    this.listDataSources = (query, callback) => {
+      this.datasource.getDataSources()
+      .then(callback);
+    };
+
   }
+
+
+
 
 }
