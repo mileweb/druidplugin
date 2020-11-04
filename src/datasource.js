6bc867f4e04239a4f9e75447da4367b565d6f05b
ZhanHui-Xu <zhanhui.xu@quantil.com>
2020-11-04 11:10:19 +0800

    delete filter whose value is any value

--- src/datasource.js
+++ src/datasource.js
@@ -482,8 +482,14 @@ function (angular, _, dateMath, moment) {
       })
       .filter(function(filter){
         //delete filter whose values is any value.
-        return !((filter.type === "in" && filter.values.length === 1 && filter.values[0] === "*") || 
-                 (filter.type === "selector" && filter.value === "*"))
+        if(filter.type === "in"){
+          return !(filter.values.length === 1 && filter.values[0] === "*");
+        }else if(filter.type === "selector"){
+          return !(filter.value === "*");
+        }else {
+          return true;
+        }
+
       })
       .map(function (filter) {
         var finalFilter = _.omit(filter, 'negate');
